183c6efc6b1989523703f5ffe2ef15a1de005b87
Jun-Hyuk Woo <woojh3690@naver.com>
2020-03-24 12:50:58 +0900

    Update 함수 구현
    Tensor 라이브러리 수정에 따른 코드 수정

--- EinsNN/include/opt/AdamOptimizer.h
+++ EinsNN/include/opt/AdamOptimizer.h
@@ -3,15 +3,17 @@
 
 #include "Optimizer.h"
 #include <Tensor.h>
+#include <unordered_map>
 
 namespace EinsNN
 {
 	class AdamOptimizer : public Optimizer
 	{
 	private:
-		double m_beta1;
-		double m_beta2;
 		double m_epsilon;
+		double m_beta1, m_beta2;
+		double m_beta1t, m_beta2t;
+		std::unordered_map<const TensorD*, TensorD> m_hist_m, m_hist_v;
 
 	public:
 		AdamOptimizer(
@@ -26,17 +28,46 @@ namespace EinsNN
 		}
 		~AdamOptimizer() {};
 
-	public:
-		/*void set_Learning_Rate(double learning_rate)
-		{
-			m_learning_rate = learning_rate;
-		}*/
 	private:
 		void init(double beta1, double beta2, double epsilon)
 		{
 			m_beta1 = beta1;
 			m_beta2 = beta2;
+			m_beta1t = beta1;
+			m_beta2t = beta2;
 			m_epsilon = epsilon;
+		}
+
+	public:
+		void update(TensorD& dvec, TensorD& tsr) override
+		{
+			TensorD* temp_dvec = &dvec;
+			TensorD* temp_tsr = &tsr;
+			TensorD& mvec = m_hist_m[&dvec];
+			TensorD& vvec = m_hist_v[&dvec];
+
+			if (mvec.shape() == vector<int>())
+			{
+				mvec.resize(dvec.shape());
+				mvec.fill(1);
+			}
+
+			if (vvec.shape() == vector<int>())
+			{
+				vvec.resize(dvec.shape());
+				vvec.fill(1);
+			}
+
+			mvec = m_beta1 * mvec + (1 - m_beta1) * dvec;
+			vvec = m_beta2 * vvec + (1 - m_beta2) * dvec.pow();
+
+			TensorD mvec_hat = mvec / (1 - m_beta1t);
+			TensorD vvec_hat = vvec / (1 - m_beta2t);
+
+			tsr = tsr - (m_lrate * mvec_hat / (vvec_hat.sqrt() + m_epsilon));
+
+			m_beta1t = m_beta1t * m_beta1;
+			m_beta2t = m_beta2t * m_beta2;
 		}
 	};
 }
