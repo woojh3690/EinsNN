9a5130e2eb883884907e52f180e5f857b20960d2
Jun-Hyuk Woo <woojh3690@naver.com>
2020-03-25 17:40:25 +0900

    필요 없는 주석 제거

--- EinsNN/include/activation/ELU.h
+++ EinsNN/include/activation/ELU.h
@@ -9,36 +9,20 @@ namespace EinsNN
 {
 	class ELU : public Activation
 	{
-	private:
-		TensorD m_zero;
-		TensorD m_one;
-		TensorD m_001;
-
 	public:
 		ELU() : Activation("ELU")
-		{
-			m_zero.append(0);
-			m_one.append(1);
-			m_001.append(0.01);
-		};
-		~ELU() {};
+		{}
+		~ELU() {}
 
 	public:
 		TensorD activate(const TensorD& Z) override
 		{
-			Tensor<bool> boolTsr = (Z > 0);
-			TensorD exp_minuse = (Z.exp() - 1);
-			TensorD right = exp_minuse * 0.01;
-			TensorD a = boolTsr.select(Z, right);
-			return a;
+			return (Z > 0).select(Z, (Z.exp() - 1) * 0.01);
 		}
 
 		TensorD apply_jacobian(const TensorD& Z, const TensorD& A, 
 			const TensorD& F) override
 		{
-			/*G.array() = (A.array() > Scalar(0)).select(
-				F, (A.array() + Scalar(0.01)) * F.array()
-			);*/
 			return (A > 0).select(F, (A + 0.01) * F);
 		}
 	};
